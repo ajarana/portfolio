644e95f244392186a1e3d58139a9471ba58f0b13
andres <ajarana@email.wm.edu>
2017-04-28 18:58:27 -0400

    right vertical scrollbar is always present to prevent moving mobile menu icon

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,12 +1,14 @@
 (function showMenu() {
   var mobileButton = document.getElementById("mobileButton");
   var html = document.getElementsByTagName("html");
-  
+
   mobileButton.onclick = function() {
     if (html[0].classList.contains("expanded")) {
       html[0].classList.remove("expanded");
+      // console.log("html contained expanded, and it was removed");
     } else {
       html[0].classList.add("expanded");
+      // console.log("html did NOT contain expanded, and it was added");
     }
   }
 })();
