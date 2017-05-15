afb9d49589a00b303d2a1799ca70d8175e93b999
andres <ajarana@email.wm.edu>
2017-05-15 00:13:13 -0400

    fixed landscape mobile menu

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -5,13 +5,8 @@
   mobileButton.onclick = function() {
     if (html[0].classList.contains("expanded")) {
       html[0].classList.remove("expanded");
-      // console.log("html contained expanded, and it was removed");
     } else {
       html[0].classList.add("expanded");
-      // console.log("html did NOT contain expanded, and it was added");
     }
   }
-
-  var rofl = mobileButton.getBoundingClientRect();
-  console.log(rofl);
 })();
