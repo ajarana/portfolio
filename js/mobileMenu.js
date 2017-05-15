aa5f50aff18d6c6b6e1bbe2d7653825d527b07d4
andres <ajarana@email.wm.edu>
2017-05-15 01:50:54 -0400

    Visibility ay changes to hide links when mobile menu is collapsed. Fixed opacity, background-color, width, height, top, right transitions across all platforms.

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,12 +1,35 @@
 (function showMenu() {
   var mobileButton = document.getElementById("mobileButton");
   var html = document.getElementsByTagName("html");
+  var headerMenu = document.getElementById("headerMenu");
+
+  var minMenuHeight = '470';
+
 
   mobileButton.onclick = function() {
+    headerMenu.classList.add("transitionEverything");
+
     if (html[0].classList.contains("expanded")) {
-      html[0].classList.remove("expanded");
+      html[0].classList.remove("expanded", "staticHeight");
+    }
+    else if (window.innerHeight < minMenuHeight) {
+      console.log('first else active');
+      html[0].classList.add("expanded", "staticHeight");
     } else {
+      console.log('last else active');
       html[0].classList.add("expanded");
     }
   }
+
+  window.addEventListener("resize", function() {
+    headerMenu.classList.remove("transitionEverything");
+
+    if (window.innerHeight < minMenuHeight) {
+      console.log('first else active');
+      html[0].classList.add("staticHeight");
+    } else {
+      console.log('last else active');
+      html[0].classList.remove("staticHeight");
+    }
+  }, false);
 })();
