2124984f9754435f83c73f5f9f72029b2794d81a
andres <ajarana@email.wm.edu>
2017-07-03 23:57:06 -0400

    Removed useless hero image, re-made introduction, adjusted spacings on mobile menu, adjusted introduction link icon positions.

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -3,41 +3,27 @@
   var html = document.getElementsByTagName("html");
   var headerMenu = document.getElementById("headerMenu");
 
-  // var minMenuHeight = '555';
-
-
   mobileButton.onclick = function() {
-    // headerMenu.classList.add("transitionEverything");
-    // headerMenu.classList.add("transitionExpand");
 
     if (html[0].classList.contains("expanded")) {
       headerMenu.classList.add("transitionCollapse");
       headerMenu.classList.remove("transitionExpand");
-      html[0].classList.remove("expanded", "staticHeight");
+      html[0].classList.remove("expanded");
     }
-    // else if (window.innerHeight < minMenuHeight) {
-    //   html[0].classList.add("expanded", "staticHeight");
-    // }
     else {
       headerMenu.classList.add("transitionExpand");
       headerMenu.classList.remove("transitionCollapse");
       html[0].classList.add("expanded");
+      console.log("transitionExpand should be added.");
     }
   }
 
   window.addEventListener("resize", function() {
-    // headerMenu.classList.remove("transitionEverything");
     headerMenu.classList.remove("transitionExpand");
     headerMenu.classList.remove("transitionCollapse");
 
     if (window.innerWidth >= 768) {
       html[0].classList.remove("expanded");
     }
-
-    // if (window.innerHeight < minMenuHeight) {
-    //   html[0].classList.add("staticHeight");
-    // } else {
-    //   html[0].classList.remove("staticHeight");
-    // }
   }, false);
 })();
